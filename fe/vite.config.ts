ea588f3d255da236e95c09f1f9e46f506d830797
mario <codemario318@gmail.com>
2023-12-28 11:54:41 +0900

    [ADD] alias 추가

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -17,5 +18,10 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+    ]
   }
 })
