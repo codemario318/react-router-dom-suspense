1302d8d63a7a3429797733a833dfd598905f4c3b
mario <codemario318@gmail.com>
2023-12-28 11:57:07 +0900

    [UPDATE] 라우터 구조 수정

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import {StrictMode, Suspense, lazy} from 'react'
 import ReactDOM from 'react-dom/client';
-import Root from './routers/root';
+import Root from '@/routers/root';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./css/index.css";
 
-import { booksLoader } from "./loaders/books-loader";
+import HomeIndex from '@/routers/home/index';
+import {loader as searchBooksLoader} from '@/routers/home/search-result'
 
+const SearchResult = lazy(() => import('@/routers/home/search-result'));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        element: <HomeIndex />,
+        children: [
+          {
+            index: true,
+            element: <SearchResult />,
+            loader: searchBooksLoader
+          }
+        ],
+      }
+    ]
   }
-]);
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </StrictMode>,
 )
